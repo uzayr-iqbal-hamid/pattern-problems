526d9f89c09a749e596b65689f3ed7052e1041c9
Uzayr Iqbal Hamid <134723279+uzayr-iqbal-hamid@users.noreply.github.com>
2024-11-12 23:09:24 +0530

    Delete Python directory

--- Python/14-pattern-increasing-letter-triangle.py
+++
@@ -1,8 +0,0 @@
-def nLetterTriangle(n):
-    for i in range(n):
-        for ch in range(ord('A'), ord('A') + i + 1):
-            print(chr(ch), end=" ")
-        print()
-
-n = int(input())
-nLetterTriangle(n)
--- Python/15-pattern-reverse-letter-triangle.py
+++
@@ -1,8 +0,0 @@
-def nLetterTriangle(n):
-    for i in range(n, 0, -1):
-        for ch in range(ord('A'), ord('A') + i):
-            print(chr(ch), end=" ")
-        print()
-
-n = int(input())
-nLetterTriangle(n)
